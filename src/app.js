df8c07fb4b7a18cabb2beae8dcb3a6748c3eb673
PRIMED587 <priveram87@gmail.com>
2025-04-15 20:35:07 +0000

    proyecto validacion

--- src/app.js
+++ src/app.js
@@ -8,52 +8,175 @@ import { Alert } from "bootstrap";
 
 window.onload = function () {
   var formulario = document.querySelector("#formulario")
-  formulario.addEventListener("submit",validar)  
-}
+  formulario.addEventListener("submit", validar)
+  document.getElementById("alerta").style.display = "none"
+} 
 
 function validar(event) {
   event.preventDefault()
   const inputCard = document.getElementById('cardnumber').value
   if (inputCard == "") {
     console.log("el campo card esta vacio");
-  }
-  
-  const inputCVC = document.getElementById('CVC').value
-  if (inputCVC == "") {
-    console.log("el campo CVC esta vacio");
-  }
-  const inputamount = document.getElementById('amount').value
-  if (inputamount == "") {
-    console.log("el campo amount esta vacio");
-  } 
+    document.getElementById("alerta").style.display = "block"
 
-  const inputfirstname = document.getElementById('firstname').value
-  if (inputfirstname == "") {
-    console.log("el campo firstname esta vacio");
-  }
+    document.getElementById("cardnumber").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+    document.getElementById("cardnumber").style.background = "rgba(255, 0, 0, 0.14)"
 
-  const inputlastname = document.getElementById('lastname').value
-  if (inputlastname == "") {
-    console.log("el campo lastname esta vacio");
-  }
+  } else {
+    document.getElementById("cardnumber").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+    document.getElementById("cardnumber").style.background = "rgba(255, 255, 255, 0.14)"
+     console.log(inputCard) 
+     document.getElementById("alerta").style.display = "none"
+    }
+     
+      const inputCVC = document.getElementById('CVC').value
+      if (inputCVC == "") {
+        console.log("el campo CVC esta vacio");
+        document.getElementById("alerta").style.display = "block"
+        document.getElementById("CVC").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+        document.getElementById("CVC").style.background = "rgba(255, 0, 0, 0.14)"
+      }
+      else {
+        document.getElementById("CVC").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+        document.getElementById("CVC").style.background = "rgba(255, 255, 255, 0.14)"
+         console.log(inputCVC) 
+      document.getElementById("alerta").style.display = "none"
+        }
 
-  const inputcity = document.getElementById('city').value
-  if (inputcity == "") {
-    console.log("el campo city esta vacio");
-  }
+      const inputamount = document.getElementById('amount').value
+      if (inputamount == "") {
+        console.log("el campo amount esta vacio");
+        document.getElementById("alerta").style.display = "block"
+        document.getElementById("amount").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+        document.getElementById("amount").style.background = "rgba(255, 0, 0, 0.14)"
+      }
 
-  const inputstate = document.getElementById('state').value
-  if (inputstate == "") {
-    console.log("el campo state esta vacio");
-  }
+      else {
+        document.getElementById("amount").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+        document.getElementById("amount").style.background = "rgba(255, 255, 255, 0.14)"
+         console.log(inputamount) 
+      document.getElementById("alerta").style.display = "none"
+        }
 
-  const inputpostalcode = document.getElementById('postalcode').value
-  if (inputpostalcode == "") {
-    console.log("el campo postalcode esta vacio");
-  }
+      const inputfirstname = document.getElementById('firstname').value
+      if (inputfirstname == "") {
+        console.log("el campo firstname esta vacio");
+        document.getElementById("alerta").style.display = "block"
+        document.getElementById("firstname").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+        document.getElementById("firstname").style.background = "rgba(255, 0, 0, 0.14)"
+      }
+
+      else {
+        document.getElementById("firstname").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+        document.getElementById("firstname").style.background = "rgba(255, 255, 255, 0.14)"
+         console.log(inputfirstname) 
+      document.getElementById("alerta").style.display = "none"
+        }
+
+      const inputlastname = document.getElementById('lastname').value
+      if (inputlastname == "") {
+        console.log("el campo lastname esta vacio");
+        document.getElementById("alerta").style.display = "block"
+        document.getElementById("lastname").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+        document.getElementById("lastname").style.background = "rgba(255, 0, 0, 0.14)"
+      }
+
+      else {
+        document.getElementById("lastname").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+        document.getElementById("lastname").style.background = "rgba(255, 255, 255, 0.14)"
+         console.log(inputlastname) 
+      document.getElementById("alerta").style.display = "none"
+        }
+
+      const inputcity = document.getElementById('city').value
+      if (inputcity == "") {
+        console.log("el campo city esta vacio");
+        document.getElementById("alerta").style.display = "block"
+        document.getElementById("city").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+        document.getElementById("city").style.background = "rgba(255, 0, 0, 0.14)"
+      }
+
+      else {
+        document.getElementById("city").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+        document.getElementById("city").style.background = "rgba(255, 255, 255, 0.14)"
+         console.log(inputcity) 
+      document.getElementById("alerta").style.display = "none"
+        }
+
+      const inputstate = document.getElementById('state').value
+      if (inputstate == "") {
+        console.log("el campo state esta vacio");
+        document.getElementById("alerta").style.display = "block"
+        document.getElementById("state").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+        document.getElementById("state").style.background = "rgba(255, 0, 0, 0.14)"
+      }
+      else {
+        document.getElementById("state").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+        document.getElementById("state").style.background = "rgba(255, 255, 255, 0.14)"
+         console.log(inputstate) 
+      document.getElementById("alerta").style.display = "none"
+        }
+
+      const inputpostalcode = document.getElementById('postalcode').value
+      if (inputpostalcode == "") {
+        console.log("el campo postalcode esta vacio");
+        document.getElementById("alerta").style.display = "block"
+        document.getElementById("postalcode").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+        document.getElementById("postalcode").style.background = "rgba(255, 0, 0, 0.14)"
+      }
+      else {
+        document.getElementById("postalcode").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+        document.getElementById("postalcode").style.background = "rgba(255, 255, 255, 0.14)"
+         console.log(inputpostalcode) 
+      document.getElementById("alerta").style.display = "none"
+        }
+
+        const exampleFormControlTextarea1 = document.getElementById('exampleFormControlTextarea1').value
+      if (exampleFormControlTextarea1 == "") {
+        console.log("el campo postalcode esta vacio");
+        document.getElementById("alerta").style.display = "block"
+        document.getElementById("exampleFormControlTextarea1").style.border = "1px solid rgba(255, 0, 0, 0.17)"
+        document.getElementById("exampleFormControlTextarea1").style.background = "rgba(255, 0, 0, 0.14)"
+      }
+      else {
+        document.getElementById("exampleFormControlTextarea1").style.border = "1px solid rgba(169, 169, 169, 0.87)"
+        document.getElementById("exampleFormControlTextarea1").style.background = "rgba(255, 255, 255, 0.14)"
+         console.log(exampleFormControlTextarea1) 
+      document.getElementById("alerta").style.display = "none"
+        }
+      
 
 
+    }
+
+/* let formsubmit = document.getElementsByClassName('row')
+formsubmit.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  console.log("Formulario Enviado")
+
+  let { cardnumber, CVC, amount, firstname, lastname, city, state, postalcode } = e.target
+
+  console.log(cardnumber.value)
+    console.log(CVC.value)
+    console.log(amount.value)
+    console.log(firstname.value)
+    console.log(lastname.value)
+    console.log(city.value)
+    console.log(state.value)
+    console.log(postalcode.value)
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*write your code here
